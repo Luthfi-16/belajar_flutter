a69d1ed61648250bb49cc617d6c6239bd908f0db
Luthfi-16 <Luffy160408@gmail.com>
2025-07-07 08:18:59 +0700

    pkl

--- lib/row_column/latihan_tiga.dart
+++ lib/row_column/latihan_tiga.dart
@@ -41,15 +41,78 @@ class LatihanTiga extends StatelessWidget {
           ),
           const SizedBox(height: 16),
 
-          Row(
-            children: [
-              Column(
-                children: [
-                  Container()
-                ],
-              )
-            ],  
-          )
+          // Gambar 2 dan 3 sejajar
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 100,
+                        margin: const EdgeInsets.only(right: 5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: Colors.white, width: 2),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 6,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                          image: const DecorationImage(
+                            image: NetworkImage(
+                                'https://variety.com/wp-content/uploads/2017/02/spidermanavengersdowney.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 100,
+                        margin: const EdgeInsets.only(left: 5),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          border: Border.all(color: Colors.white, width: 2),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black26,
+                              blurRadius: 6,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                          image: const DecorationImage(
+                            image: NetworkImage(
+                                'https://s.abcnews.com/images/Entertainment/ht-tom-holland-spider-man-jc-2-170626_16x9_992.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+  
+                // Gambar 4
+                Container(
+                  height: 150,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: Colors.white, width: 2),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black26,
+                        blurRadius: 6,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                    image: const DecorationImage(
+                      image: NetworkImage(
+                          'https://occ-0-8407-92.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABdfGo9D_DveXMM8KK8KspcyJfgdsf_UuNz4rFnSVZ24Gtq1gBPI_vUGRBn3qSZc0VQoJabPCEctzMcpULKkw-zq5VsZFfvUI3HOT.jpg?r=147'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
         ],
       ),
     );
